77e8506aaaae8372f6ac71b4b85fca853d6d95f6
ravitejane <raviteja.g689@gmail.com>
2017-12-18 22:39:45 +0530

    commit

--- teja/src/main/java/teja/com/model/Admin.java
+++ teja/src/main/java/teja/com/model/Admin.java
@@ -1,8 +1,57 @@
 package teja.com.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="admin")
 public class Admin {
-
+	
+	@Id
+	private String id;
+	private String name;	
+	private Date dateOfBirth;
+	private String email;
+	private String gender;
+	private long phoneNo;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	
 }
