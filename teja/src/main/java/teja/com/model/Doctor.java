4c1db7db86eb0bf02b15fa522693ef560846f8a5
ravitejane <raviteja.g689@gmail.com>
2017-12-19 14:28:02 +0530

    ok

--- teja/src/main/java/teja/com/model/Doctor.java
+++ teja/src/main/java/teja/com/model/Doctor.java
@@ -1,8 +1,18 @@
 package teja.com.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="doctor")
 public class Doctor {
+	private String dId;
+	private String name;
+	private long phoneNo;
+	private String address;
+	private String specialization;
+	private String prescription;
+	
+	
 
 }
